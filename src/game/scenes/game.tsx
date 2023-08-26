b186a365e01c03bf285e8d53e62bc21740d38e0e
Daniel Jorge <djpgonzaga@gmail.com>
2023-08-26 00:34:55 -0300

    feat: load hero assets and define moviment animations

--- src/game/scenes/game.tsx
+++ src/game/scenes/game.tsx
@@ -17,11 +17,68 @@ export class Game extends Phaser.Scene {
     if (!walls) throw new Error("Walls layer not found");
 
     walls.setCollisionByProperty({ collides: true });
-    walls.renderDebug(this.add.graphics().setAlpha(0.5), {
-      tileColor: null,
-      faceColor: new Phaser.Display.Color(40, 39, 37, 255),
-      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
+    // walls.renderDebug(this.add.graphics().setAlpha(0.5), {
+    //   tileColor: null,
+    //   faceColor: new Phaser.Display.Color(40, 39, 37, 255),
+    //   collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
+    // });
+
+    const hero = this.add.sprite(100, 60, "hero", "walk-down-0.png");
+
+    this.anims.create({
+      key: "hero-idle-down",
+      frames: [{ key: "hero", frame: "walk-down-0.png" }],
     });
+    this.anims.create({
+      key: "hero-walk-down",
+      frames: this.anims.generateFrameNames("hero", {
+        start: 0,
+        end: 3,
+        prefix: "walk-down-",
+        suffix: ".png",
+      }),
+      repeat: -1,
+      frameRate: 8,
+    });
+    this.anims.create({
+      key: "hero-walk-up",
+      frames: this.anims.generateFrameNames("hero", {
+        start: 0,
+        end: 3,
+        prefix: "walk-up-",
+        suffix: ".png",
+      }),
+      repeat: -1,
+      frameRate: 8,
+    });
+    this.anims.create({
+      key: "hero-walk-left",
+      frames: this.anims.generateFrameNames("hero", {
+        start: 0,
+        end: 3,
+        prefix: "walk-left-",
+        suffix: ".png",
+      }),
+      repeat: -1,
+      frameRate: 8,
+    });
+    this.anims.create({
+      key: "hero-walk-right",
+      frames: this.anims.generateFrameNames("hero", {
+        start: 0,
+        end: 3,
+        prefix: "walk-right-",
+        suffix: ".png",
+      }),
+      repeat: -1,
+      frameRate: 8,
+    });
+
+    hero.anims.play("hero-walk-idle");
+    // hero.anims.play("hero-walk-down");
+    // hero.anims.play("hero-walk-up");
+    // hero.anims.play("hero-walk-left");
+    // hero.anims.play("hero-walk-right");
   }
 
   update() {}
--- src/game/scenes/preloader.ts
+++ src/game/scenes/preloader.ts
@@ -8,6 +8,11 @@ export class Preloader extends Phaser.Scene {
   preload() {
     this.load.image("tiles", "assets/tiles/overworld.png");
     this.load.tilemapTiledJSON("world", "assets/tiles/world-01.json");
+    this.load.atlas(
+      "hero",
+      "assets/character/texture.png",
+      "assets/character/texture.json"
+    );
   }
   create() {
     this.scene.start("game");
